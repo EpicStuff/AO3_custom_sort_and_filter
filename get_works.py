4a793682f586bf89251629e8735e0a6eb712302f
EpicStuff <EpicStuff@users.noreply.github.com>
2026-03-25 11:07:50 -0400

    updated get_works progress bar

--- get_works.py
+++ get_works.py
@@ -15,7 +15,7 @@ Options:
 	-i, --input [<input>]       File to merge new works with.
 	-I                          Use same input file as output file. Do not use -i with this flag.
 
-	-n, --num-to-get <num>      Number of works to get. Defaults to all.
+	-w, --works-to-get <works>  Number of works to get. Defaults to all.
 	-p, --pages-to-get <pages>  Number of pages to get. Defaults to all.
 
 	-s, --skip <skip>           File containing works to skip.
@@ -24,14 +24,19 @@ Options:
 '''
 import csv, time
 from pathlib import Path
+from functools import cache
 
 import requests
 from bs4 import BeautifulSoup
 from docopt import docopt
-from epicstuff import Dict
-from rich.progress import Progress
+from epicstuff import Dict, Bar
+from rich.progress import TextColumn
 from stuff import load_csv
 
+
+@cache
+def get_request(url: str) -> requests.Response:
+	return requests.get(url, timeout=60)
 
 def update_url_to_next_page(args: Dict) -> None:
 	key = 'page='
@@ -64,11 +69,11 @@ def write_ids_to_csv(args: Dict, stuff: Dict, ids: list[dict]) -> None:
 		for id_ in ids:
 			if is_done(args, stuff): break
 			writer.writerow([val for _, val in id_.items()])
-			stuff.num_gotten += 1
+			stuff.works_gotten += 1
 	stuff.pages_gotten += 1
 def get_ids(args: Dict, stuff: Dict, seen_ids: set[str]) -> list[dict]:
 	# make the request. if we 429, try again later
-	req = requests.get(args.url)
+	req = get_request(args.url)
 	while req.status_code == 429:
 		time.sleep(args.delay)
 		req = requests.get(args.url)
@@ -100,16 +105,16 @@ def get_ids(args: Dict, stuff: Dict, seen_ids: set[str]) -> list[dict]:
 def is_done(args: Dict, stuff: Dict) -> bool:
 	return \
 		stuff.is_page_empty or \
-		(args.num_to_get > 0 and stuff.num_gotten >= args.num_to_get) or \
+		(args.works_to_get > 0 and stuff.works_gotten >= args.works_to_get) or \
 		(args.pages_to_get > 0 and stuff.pages_gotten >= args.pages_to_get)
 def create_file(file: Path, to_write: list[str]) -> None:
-	print('no existing file; creating new file...\n')
+	print('creating/overwriting file\n')
 	with file.open('w', newline='', encoding='utf8') as f:
 		writer = csv.writer(f, delimiter=',')
 		writer.writerow(to_write)
 def main() -> None:  # noqa: C901, PLR0912
 	stuff = Dict({
-		'num_gotten': 0,
+		'works_gotten': 0,
 		'pages_gotten': 0,
 		'is_page_empty': False,
 		'to_get': {
@@ -127,7 +132,7 @@ def main() -> None:  # noqa: C901, PLR0912
 	args.output = Path(args.output)
 	if not args.output.suffix:
 		args.output = args.output.with_suffix('.csv')
-	args.num_to_get = int(args.num_to_get) if args.num_to_get else -1
+	args.works_to_get = int(args.works_to_get) if args.works_to_get else -1
 	args.pages_to_get = int(args.pages_to_get) if args.pages_to_get else -1
 	args.delay = int(args.delay)
 
@@ -141,30 +146,63 @@ def main() -> None:  # noqa: C901, PLR0912
 	if args.input is None or not args.output.exists() or not args.output.stat().st_size:
 		create_file(args.output, list(stuff.to_get))
 
-	with Progress() as progress:
-		bars = {}
-		if args.num_to_get > 0:
-			bars['num'] = [progress.add_task('works gotten: ', total=args.num_to_get), 0]
-		if args.pages_to_get > 0:
-			bars['pages'] = [progress.add_task('pages processed: ', total=args.pages_to_get), 0]
-		elif not args.num_to_get > 0:
-			progress.add_task('processing: ', total=None)
-		while not is_done(args, stuff):
+	# scrape ao3 with progress bars
+	# bar1 is for main bar with MofN, bar2 is for sub bars without MofN
+	Bar1 = Bar()
+	with Bar1 as bar1, Bar(add_columns={0: TextColumn('\t')}, remove_columns=[1]) as bar2:
+		# get total number of pages/works
+		tmp_bar = bar2(range(1), 'Working', None, True)
+		next(tmp_bar)
+		req = get_request(args.url)
+		req.raise_for_status()
+		soup = BeautifulSoup(req.text, 'lxml')
+		soup = soup.find('ol', class_='pagination').find_all('li')  # pyright: ignore[reportOptionalMemberAccess]
+		if soup[-1].get('class') == ['next']:
+			del soup[-1]
+		pages = int(soup[-1].find('a').text)  # pyright: ignore[reportOptionalMemberAccess]
+
+		soup = BeautifulSoup(req.text, 'lxml').find('h2', class_='heading')
+		works = int(soup.text.split()[4].replace(',', ''))  # pyright: ignore[reportOptionalMemberAccess]
+
+		next(tmp_bar, None)
+
+		# determine if pages or works is the stopping factor, assumes 20 works per page
+		if args.works_to_get > 0 and args.pages_to_get > 0:
+			limit = 'pages' if args.pages_to_get * 20 < args.works_to_get else 'works'
+		elif args.works_to_get < 0 and args.pages_to_get < 0:
+			limit = 'pages'
+		else:
+			limit = 'pages' if args.pages_to_get > 0 else 'works'
+
+		if limit == 'pages':
+			main_bar = bar1(range(min(pages, args.pages_to_get) if args.pages_to_get > 0 else pages), 'Page:', cycle=False)
+		else:
+			main_bar = bar1(range(min(works, args.works_to_get) if args.works_to_get > 0 else works), 'Work:', cycle=False)
+		next(main_bar)
+
+		# loop until reached specified number of pages/works or all works have been gotten
+		while True:
+			# actual scraping
+			request_bar = bar2(range(1), 'Requesting page', None, True)
+			next(request_bar)
 			write_ids_to_csv(args, stuff, get_ids(args, stuff, seen))
 			update_url_to_next_page(args)
+			next(request_bar, None)
 
-			if 'num' in bars:
-				bars['num'][1] = stuff.num_gotten - progress.tasks[bars['num'][0]].completed
-			if 'pages' in bars:
-				bars['pages'][1] = stuff.pages_gotten - progress.tasks[bars['pages'][0]].completed
+			# more progress bar stuff
+			if limit == 'pages':
+				next(main_bar, None)
+			else:
+				Bar1.progress.update(Bar1.tasks[0], completed=stuff.works_gotten) # pyright: ignore[reportOptionalMemberAccess]
 
+			if is_done(args, stuff):
+				break
+			# else:
 			steps = 100
-			for _ in range(steps):
-				if 'num' in bars:
-					progress.update(bars['num'][0], advance=bars['num'][1] / steps)
-				if 'pages' in bars:
-					progress.update(bars['pages'][0], advance=bars['pages'][1] / steps)
-				time.sleep(args['delay'] / steps)
+			for _ in bar2(range(steps), 'Sleeping', transient=True):
+				time.sleep(args.delay / steps)
+
+		#next(main_bar, None)
 
 
 if __name__ == '__main__':
